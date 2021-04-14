78344a4c93aa3b8973a7ae27e86975ee74e58ec1
danielclas <danielclas@outlook.es>
2021-04-13 23:50:37 -0300

    Added geocoder dependency and middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -93,6 +95,30 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+});
+
+//Middleware that runs before the 'save' action is carried by mongoose
+BootcampSchema.pre('save', function(next){
+    this.slug = slugify(this.name, {lower: true});
+    next();
+});
+
+//Geocode and create location field
+BootcampSchema.pre('save', async function(next){
+    const location = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [location[0].longitude, location[0].latitude],
+        formattedAddress: location[0].formattedAddres,
+        street: location[0].streetName,
+        city: location[0].city,
+        state: location[0].stateCode,
+        zipcode: location[0].zipcode,
+        country: location[0].countryCode,        
+    }
+
+    this.address = undefined;
+    next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
---
+++ utils/geocoder.js
@@ -0,0 +1,8 @@
+const nodeGeocoder = require('node-geocoder');
+
+module.exports = nodeGeocoder({
+    provider: process.env.GEOCODER_PROVIDER,
+    httpAdapter: 'https',
+    apiKey: process.env.GEOCODER_API_KEY,
+    formatter: null
+});
