22a1032c1eaf04f1f674ec9caa350869ac2f2ea4
danielclas <danielclas@outlook.es>
2021-04-18 10:00:17 -0300

    Added update details route, controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
+const { updateDetails, register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/forgotpassword', forgotPassword);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
 router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
